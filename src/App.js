1ba03294be2e8b597d05b3873fca7c978ec62633
Iliya Baghjari <70961525+alibaghjari145@users.noreply.github.com>
2022-11-22 03:04:14 +0330

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [state, setState] = useState(0);
+  const [num, setNum] = useState("");
+
+  function createNum() {
+    let x = Math.floor(Math.random() * 6) + 96;
+    if (x > 99) {
+      x = x + 300;
+    }
+    let y = Math.floor(Math.random() * 100) + 1;
+    setNum(x + "280" + y);
+  }
+
+  function calc() {
+    setState(1);
+    createNum();
+    const myTime = setTimeout(endLoad, 2000);
+  }
+  function endLoad() {
+    setState(2);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {state === 0 && (
+          <div className="default">
+            <p>
+              If you want to have a Nassaj crush and you can not choose, we will
+              help you.
+              <br />
+              <br />
+              if number does not exist, you will be "SINGLE BE GOOR"
+            </p>
+            <button onClick={calc}>click me</button>
+          </div>
+        )}
+        {state === 2 && (
+          <div className="default">
+            <p>
+              Your crush's number is:
+              <br />
+              <br />
+              <span className="resNum">{num}</span>
+            </p>
+            <button
+              onClick={() => {
+                setState(0);
+              }}
+            >
+              AGAIN
+            </button>
+          </div>
+        )}
+        {state === 1 && (
+          <div class="ring">
+            LOADING
+            <span className="span"></span>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
